e35601520c552d138a966b1a0dd1334ae97607b5
Flavio S. de Lima <flavio.fatesg@yahoo.com.br>
2016-07-21 07:57:54 -0300

    Comite inicial

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('postagensRecentesCtrl', function($scope) {
+
+})
+   
+.controller('categoriasCtrl', function($scope) {
+
+})
+    
---
+++ www/js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('menu.postagensRecentes', {
+    url: '/page2',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/postagensRecentes.html',
+        controller: 'postagensRecentesCtrl'
+      }
+    }
+  })
+
+  .state('menu.categorias', {
+    url: '/categorias',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/categorias.html',
+        controller: 'categoriasCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/page1')
+
+  
+
+});
