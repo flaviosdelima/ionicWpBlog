dc0732e076e0a91fa391cfca77572a00a5dac77d
Flavio S. de Lima <flavio.fatesg@yahoo.com.br>
2016-07-21 09:09:39 -0300

    Recebendo informações com json

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,7 +8,18 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('categoriasCtrl', function($scope) {
+.controller('categoriasCtrl', function($scope,$http) {
+    
+    $scope.categories = [];
+    $http.get("http://192.168.15.5/wordpress43/?json=get_category_index").then(function(retdata){
+        //sucess
+        $scope.categories = retdata.data.categories;
+        console.log(retdata);
+        
+        }, function(error){
+            //erro
+            console.log(error);
+            });
 
 })
     
